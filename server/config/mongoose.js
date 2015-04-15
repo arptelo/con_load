55401577f4de55a01e93635a52ff3afa68c582e5
ArslanC9 <arslantaskin@gmail.com>
2015-04-15 10:40:20 +0000

    Add authentication and authorization

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,4 +1,5 @@
-var mongoose = require("mongoose");
+var mongoose = require("mongoose"),
+  crypto = require("crypto");
 
 module.exports = function(config){
   mongoose.connect(config.db);
@@ -11,15 +12,39 @@ module.exports = function(config){
   var userSchema = mongoose.Schema({
     firstName: String,
     lastName: String,
-    userName: String
+    userName: String,
+    salt: String,
+    hashed_pwd: String,
+    roles: [String]
   });
+  userSchema.methods = {
+    authenticate: function(passwordToMatch){
+      return hashPwd(this.salt, passwordToMatch) === this.hashed_pwd;
+    }
+  };
   
   var User = mongoose.model('User', userSchema);
   User.find({}).exec(function(err, collection){
     if(collection.length === 0){
-      User.create({firstName: 'Arslan', lastName: 'Taşkın', userName: 'arslan'});
-      User.create({firstName: 'Fatih', lastName: 'Taşkın', userName: 'fatih'});
-      User.create({firstName: 'Mustafa', lastName: 'Erbay', userName: 'mustafa'});
+      var salt, hash;
+      salt = createSalt();
+      hash = hashPwd(salt, 'arslan');
+      User.create({firstName: 'Arslan', lastName: 'Taşkın', userName: 'arslan', salt: salt, hashed_pwd: hash, roles: ['admin']});
+      salt = createSalt();
+      hash = hashPwd(salt, 'fatih');
+      User.create({firstName: 'Fatih', lastName: 'Taşkın', userName: 'fatih', salt: salt, hashed_pwd: hash});
+      salt = createSalt();
+      hash = hashPwd(salt, 'mustafa');
+      User.create({firstName: 'Mustafa', lastName: 'Erbay', userName: 'mustafa', salt: salt, hashed_pwd: hash});
     }
   });
 };
+
+function createSalt() {
+  return crypto.randomBytes(128).toString('base64');
+}
+
+function hashPwd(salt, pwd) {
+  var hmac = crypto.createHmac('sha1', salt);
+  return hmac.update(pwd).digest("hex");
+}
